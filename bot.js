c5928abf7e16d31721ff9fc101331be0e2ca2d07
Andrew Minai <andrewminai24@gmail.com>
2019-06-06 16:48:19 -0700

    Twitbot

---
+++ bot.js
@@ -0,0 +1,27 @@
+console.log("The bot is starting");
+
+var Twit = require('twit');
+
+var config = require('./config');
+var T = new Twit(config);
+
+
+
+
+
+var params = {
+    q: 'michaeljordan',
+    count:1
+}
+
+
+
+T.get('search/tweets',params,gotData);
+
+function gotData(err,data,response)
+{
+    var tweets = data.statuses;
+for (var i = 0; i < tweets.length;i++){
+    console.log(tweets[i].text);
+    }
+}
---
+++ config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    consumer_key:'',
+    consumer_secret:'',
+    access_token:'',
+    access_token_secret:''
+}
+
+//get is getting the user or simply requesting
+//post is tweeting  putting info
+//stream is being connected to the api
+
